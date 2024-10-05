bf11d9b0066a89148b9c493b321f53b5a020d304
SUMIT9958-bit <ksumit83794@gmail.com>
2024-10-05 13:36:22 +0530

    2nd part completed-ui-design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,39 @@
+//import React from 'react'
+
+import Conversation from "./Conversation"
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+
+    </div>
+  )
+}
+
+export default Conversations
+
+
+//STRATER CODE SNIPPET
+//import Conversation from "./Conversation"
+
+//const Conversations = () => {
+//  return (
+//    <div className="py-2 flex flex-col overflow-auto">
+//      <Conversation/>
+//      <Conversation/>
+//      <Conversation/>
+//      <Conversation/>
+//      <Conversation/>
+//      <Conversation/>
+
+//    </div>
+//  )
+//}
+
+//export default Conversations
