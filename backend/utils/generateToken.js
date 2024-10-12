167e7559264d664c6380f30e562fe1639d2e2415
SUMIT9958-bit <ksumit83794@gmail.com>
2024-10-12 13:40:34 +0530

    updated JSONWEBTOEKN

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 
 const generateTokenAndSetCokkie =(userId, res) =>{
-  const token = jwt.sign({userId}, process.env.jwt_SECRET,{
+  const token = jwt.sign({userId}, process.env.JWT_SECRET,{
     expiresIn: '15d',
   })
 
